04cfa54e3dc0f648eadcf06d6e6a6da1e68c58d6
Tonycrz <tonybaidoo0@gmail.com>
2021-11-16 19:34:50 +0000

    fizzbuzz

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -6,6 +6,6 @@ def fizzbuzz():
         elif i % 3 == 0:
             print("Fizz", end=" ")
         elif i % 5 == 0:
-                print("Buzz", end=" ")
+            print("Buzz", end=" ")
         else:
             print(i, end=" ")
