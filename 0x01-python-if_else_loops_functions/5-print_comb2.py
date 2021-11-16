c50643a975dd45d174ee3f00e7955442b29ca4e5
Tonycrz <tonybaidoo0@gmail.com>
2021-11-16 16:21:18 +0000

    print 00 to 99

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-for i in range(0, 10):
-    for x in range(0, 10):
-        if i == 9 and x == 9:
-            print("{0}{1}".format(i, x), end="\n")
-        else:
-            print("{0}{1}".format(i, x), end=", ")
+for i in range(0, 100):
+    if i == 99:
+        print("{:0<2}".format(i), end="\n")
+    else:
+        print("{:0<2}".format(i), end=", ")
