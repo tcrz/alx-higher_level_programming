29316807f9caca4e5f92181725fda3a54236f8e0
Tonycrz <tonybaidoo0@gmail.com>
2021-11-18 13:24:29 +0000

    print args

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+import sys
+
+if __name__ == "__main__":
+    n = len(sys.argv)
+    if n == 1:
+        print("0 arguments.")
+    elif n == 2:
+        print("1 argument:")
+        print("{}: {}".format(1, sys.argv[1]))
+    else:
+        print("{} arguments:".format(n-1))
+        for i in range(1, n):
+            print("{}: {}".format(i, sys.argv[i]))
