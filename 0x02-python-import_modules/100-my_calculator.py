21bdc5e20d4cdb930ed5cb187807283056851d8f
Tonycrz <tonybaidoo0@gmail.com>
2021-11-18 16:03:48 +0000

    update

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
-import sys
+
 from calculator_1 import add, sub, mul, div
 
 if __name__ == "__main__":
+    import sys
     def calc(func, a, b, sign):
         c = func(a, b)
-        print("{:d} {} {:d} = {:d}".format(a, sign, b, c))
+        print("{:d} {:s} {:d} = {:d}".format(a, sign, b, c))
 
-    if len(sys.argv) != 4:
+    if len(sys.argv) - 1 != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
 
