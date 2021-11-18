15c184e705099b7d83ad28a4104e9f0e5ec10e29
Tonycrz <tonybaidoo0@gmail.com>
2021-11-18 14:01:48 +0000

    print names defined in module

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -5,5 +5,5 @@ if __name__ == "__main__":
     list = dir(hidden_4)
     n = len(dir(hidden_4))
     for i in range(n):
-       if list[i][:2] != "__":
-           print(list[i])
+        if list[i][:2] != "__":
+            print(list[i])
