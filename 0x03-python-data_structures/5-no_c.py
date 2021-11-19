02d3335f6573abfdd4a473a3abcf2d37e637e50c
Tonycrz <tonybaidoo0@gmail.com>
2021-11-19 14:07:59 +0000

    remove all Cs

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def no_c(my_string):
+    newstring = ""
+    for character in my_string:
+        if character not in 'Cc':
+            newstring += character
+    return newstring
