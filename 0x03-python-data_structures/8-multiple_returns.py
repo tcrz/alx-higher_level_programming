ba56c38c1b352ed722bee6fac5a18cb778fd2e56
Tonycrz <tonybaidoo0@gmail.com>
2021-11-20 11:33:11 +0000

    minor change

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -4,7 +4,9 @@ def multiple_returns(sentence):
 
     length = len(sentence)
     if length != 0:
-        tuple = length, sentence[0]
+        str = sentence
+        tuple = length, str[0]
     else:
-        tuple = length, "None"
+        str = None
+        tuple = length, str
     return tuple
