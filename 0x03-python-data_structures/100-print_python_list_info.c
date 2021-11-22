445537175ed149a7ebbc7ca66052bf8abfde7227
Tonycrz <tonybaidoo0@gmail.com>
2021-11-22 16:30:24 +0000

    CPython

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,27 @@
+#include <Python.h>
+
+void print_python_list_info(PyObject *p)
+{
+
+	if (PyList_Check(p))
+	{
+		int size, i;
+		PyListObject *listobj = (PyListObject *)p;
+		PyObject *obj;
+
+		size = PyList_Size(p);
+		printf("[*] Size of the Python List = %d\n", size);
+		printf("[*] Allocated: %li\n", listobj->allocated);
+
+		i = 0;
+		while (i < size)
+		{
+			obj = PyList_GetItem(p, i);
+			printf("Element %d: %s\n", i, Py_TYPE(obj)->tp_name);
+			i++;
+		}
+	}
+}
+
+
+
