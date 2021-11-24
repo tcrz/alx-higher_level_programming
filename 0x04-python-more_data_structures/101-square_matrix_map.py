0436d7b22220a4753c44dfd229e6b80cdba9f11a
Tonycrz <tonybaidoo0@gmail.com>
2021-11-24 19:06:46 +0000

    square_matrix

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -2,5 +2,5 @@
 def square_matrix_map(matrix=[]):
     new_list = []
     for eachlist in matrix:
-            new_list.append(list(map(lambda x: x * x, eachlist)))
+        new_list.append(list(map(lambda x: x * x, eachlist)))
     return new_list
