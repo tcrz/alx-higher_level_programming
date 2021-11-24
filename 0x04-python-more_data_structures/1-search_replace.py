3b650a3eff1362f79c330e348252f5e31449537b
Tonycrz <tonybaidoo0@gmail.com>
2021-11-24 14:10:10 +0000

    find and replace

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    new_list = my_list[:]
-
-    idx = new_list.index(search)
-    new_list.pop(idx)
-    new_list.insert(idx, replace)
+    new_list = []
+    for i in my_list:
+        if i != search:
+            new_list.append(i)
+        else:
+            i = replace
+            new_list.append(i)
+    # idx = new_list.index(search)
+    # new_list.pop(idx)
+    # new_list.insert(idx, replace)
     return new_list
