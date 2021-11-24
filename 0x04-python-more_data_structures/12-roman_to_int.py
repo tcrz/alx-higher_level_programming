42f78871120347dd65631044bce0d35f31bc56d4
Tonycrz <tonybaidoo0@gmail.com>
2021-11-24 16:54:18 +0000

    new changes

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -5,4 +5,4 @@ def roman_to_int(roman_string):
         r = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
         for i in roman_string:
             total += r[i]
-    return total
+        return total
