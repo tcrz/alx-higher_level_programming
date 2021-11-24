2907b07371099352cb0adb251c14a1af3e017bff
Tonycrz <tonybaidoo0@gmail.com>
2021-11-24 14:35:52 +0000

    delete

--- 0x04-python-more_data_structures/3-common_elments.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-def common_elements(set_1, set_2):
-    return set_1 & set_2
-
-
-set_1 = { "Python", "C", "Javascript" }
-set_2 = { "Bash", "C", "Python", "Perl" }
-c_set = common_elements(set_1, set_2)
-print(sorted(list(c_set)))
