ce3e000d72bf2c40fe43c6a7c8b7efdd9a7d722e
Tonycrz <tonybaidoo0@gmail.com>
2021-11-24 18:19:01 +0000

    roman numerals

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,6 +3,16 @@ def roman_to_int(roman_string):
     total = 0
     if roman_string:
         r = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+
+    if roman_string[-2:] == 'IX' or roman_string[-2:] == 'IV':
+        for i in roman_string[:-2]:
+            total += r[i]
+        if roman_string[-2:] == 'IX':
+            total += 9
+        if roman_string[-2:] == 'IV':
+            total += 4
+    else:
         for i in roman_string:
             total += r[i]
-        return total
+
+    return total
