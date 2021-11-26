b1edf3e228c386fdf7667dae5c8eb89e0a7d15f9
Tonycrz <tonybaidoo0@gmail.com>
2021-11-26 16:03:58 +0000

    variable fixes

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,10 +3,11 @@ def roman_to_int(roman_string):
     rstr = roman_string
     total = 0
     if rstr:
-        r = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
 
     for i in range(len(rstr)):
-        if i > 0 and rstr[r[i]] > rstr[r[i - 1]]:
-            total += rstr[r[i]] - (2 * rstr[r[i - 1]])
+        if i > 0 and roman[rstr[i]] > roman[rstr[i - 1]]:
+            total += roman[rstr[i]] - (2 * roman[rstr[i - 1]])
         else:
-            total += rstr[r[i]]
+            total += roman[rstr[i]]
+    return total
