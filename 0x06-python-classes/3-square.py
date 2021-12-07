95bb1f7994cc1b0ce8a6bf4f5445d4cde79c35ad
Tonycrz <tonybaidoo0@gmail.com>
2021-12-07 20:00:59 +0000

    add area (public instance method)

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,18 @@
+"""
+Define a Square class
+"""
+
+
+class Square:
+    """A class that defines a Square with a private attribute called size"""
+    def __init__(self, size=0):
+        if type(size) is not int:
+            raise TypeError('size must be an integer')
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    """A public instance method that returens the current square area"""
+    def area(self):
+        return (self.__size ** 2)
