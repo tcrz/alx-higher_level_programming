5251236fad6d783dc6f2cf3d4ff7df46a3a45276
Tonycrz <tonybaidoo0@gmail.com>
2021-12-08 10:24:21 +0000

    updates

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -5,8 +5,8 @@
 class Square:
     """A Square Class"""
     def __init__(self, size=0, position=(0, 0)):
-        self.__size = size
-        self.__position = position
+        self.size = size
+        self.position = position
 
     def area(self):
         """method that returns area of a square"""
