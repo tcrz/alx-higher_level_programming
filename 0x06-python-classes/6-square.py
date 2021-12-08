7113b5b2299f984707275e727b32c547a0c9d47b
Tonycrz <tonybaidoo0@gmail.com>
2021-12-08 15:07:50 +0000

    test code

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,59 +1,88 @@
 #!/usr/bin/python3
-"""Define a Class"""
+"""Defining Square in class"""
 
 
 class Square:
-    """A Square Class"""
+    """This is class is to compute area of a square"""
     def __init__(self, size=0, position=(0, 0)):
-        self.size = size
-        self.position = position
+        """Class is initialized with a field named size and position.
+        Args:
+            size (int): size is a new field it has been initialized to 0.
+            position (int, int): position is a new field it has been
+            initialized
+        """
+        if not(isinstance(size, int)):
+            raise TypeError("size must be an integer")
+        elif (size < 0):
+            raise ValueError("size must be >= 0")
+        elif (not(isinstance(position, tuple)) or len(position) != 2):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif (type(position[0]) != int or type(position[1]) != int):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif (position[0] < 0 or position[1] < 0):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__size = size
+        self.__position = position
 
     def area(self):
-        """method that returns area of a square"""
-        return (self.__size ** 2)
+        """This funtion is used to find the area of square.
+        Returns:
+            size squared
+        """
+        return self.__size**2
 
     @property
     def size(self):
-        """property to retrieve attribute, size"""
+        """This function is used to return  the value that
+           was changed by the size setter function.
+        Args:
+            value (int): the value to be changed
+        Returns:
+            the new size
+        """
         return self.__size
-
-    @property
-    def position(self):
-        """property to retrieve position attribute"""
-        return self.__position
 
     @size.setter
     def size(self, value):
-        """property to set value for size attribute"""
-        if type(value) is not int:
+        if not(isinstance(value, int)):
             raise TypeError("size must be an integer")
-        elif value < 0:
+        if (value < 0):
             raise ValueError("size must be >= 0")
-        else:
-            self.__size = value
+        self.__size = value
+
+    @property
+    def position(self):
+        """This function is used to return the value that
+           was changed by the position setter function.
+        Args:
+            value (int, int): th tuple to be changed
+        Returns:
+            The new tuples
+        """
+        return self.__position
 
     @position.setter
     def position(self, value):
-        """property to set value for position attribute"""
-        if len(value) != 2:
+        if (not(isinstance(value, tuple)) or len(value) != 2):
             raise TypeError("position must be a tuple of 2 positive integers")
-        elif type(value[0]) is not int or type(value[1]) is not int:
+        elif (type(value[0]) != int or (type(value[1]) != int)):
             raise TypeError("position must be a tuple of 2 positive integers")
-        elif value[0] < 0 or value[1] < 0:
+        elif (value[0] < 0 or value[1] < 0):
             raise TypeError("position must be a tuple of 2 positive integers")
-        else:
-            self.__position = value
+        self.__position = value
 
     def my_print(self):
-        """method to print a square with position"""
-        if self.__size == 0:
+        """This function is used to print a square in form
+           of # using the value given. And tuples as space
+        """
+        if (self.__size == 0):
             print()
-            return
-        for y in range(0, self.__position[1]):
-            print()
-        for i in range(0, self.__size):
-            for x in range(0, self.__position[0]):
-                print(" ", end="")
-            for j in range(0, self.__size):
-                print("#", end="")
-            print()
+        else:
+            for k in range(0, self.__position[1]):
+                print()
+            for i in range(0, self.__size):
+                for a in range(0, self.__position[0]):
+                    print(" ", end="")
+                for j in range(0, self.__size):
+                    print("#", end="")
+                print()
