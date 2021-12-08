c52541cd7240b2354e9e0942507a9ff5ac204d05
Tonycrz <tonybaidoo0@gmail.com>
2021-12-08 00:10:10 +0000

    position method added

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+"""Define a Class"""
+
+
+class Square:
+    """A Square Class"""
+    def __init__(self, size=0, position=(0, 0)):
+        self.__size = size
+        self.__position = position
+
+    """method that returns area of a square"""
+    def area(self):
+        return (self.__size ** 2)
+
+    """property to retrieve attribute, size"""
+    @property
+    def size(self):
+        return self.__size
+
+    """property to retrieve position attribute"""
+    @property
+    def position(self):
+        return self.__position
+
+    """property to set value for size attribute"""
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    """property to set value for position attribute"""
+    @position.setter
+    def position(self, value):
+        if value[0] < 0 or value[1] < 0:
+            raise TypeError("""position must be a tuple of 2 positive \
+integers""")
+        else:
+            self.__position = value
+
+    """method to print a square with position"""
+    def my_print(self):
+        if self.__size == 0:
+            print("")
+        else:
+            if self.__position[1] > 0:
+                print("")
+            for i in range(self.__size):
+                for i in range(self.__position[0]):
+                    print(" ", end="")
+                for x in range(self.__size):
+                    print("#", end="")
+                print("")
