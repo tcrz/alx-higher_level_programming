fa5c9e799f222d86e603a918ec9a7ed672cd1b85
Tonycrz <tonybaidoo0@gmail.com>
2021-12-12 15:02:27 +0000

    prints full name

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+This is the 3-say_my_name.py module
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    """
+    prints My name is <first name> <last name>
+
+    Args:
+        first_name (str): str argument
+        last_name (str, optional): str argument. Defaults to "".
+    """
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+    if type(first_name) is str and type(last_name) is str:
+        print("My name is {} {}".format(first_name, last_name))
+
+
+if __name__ == "__main__":
+    import doctestgit sta
+    doctest.testmod()
