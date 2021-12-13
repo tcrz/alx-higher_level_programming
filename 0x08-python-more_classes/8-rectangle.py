5e8be626bb19ef1c3eacf98ef26b2efc0d5050e5
Tonycrz <tonybaidoo0@gmail.com>
2021-12-13 23:18:09 +0000

    doctring for static method

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -74,6 +74,7 @@ class Rectangle:
 
     @staticmethod
     def bigger_or_equal(rect_1, rect_2):
+        """static method returns bigger rectangle"""
         if type(rect_1) is not Rectangle:
             raise TypeError("rect_1 must be an instance of Rectangle")
         if type(rect_2) is not Rectangle:
