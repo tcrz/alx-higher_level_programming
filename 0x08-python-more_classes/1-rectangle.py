c5c9c98ecbcc434d91c510cfa16569657756b0e6
Tonycrz <tonybaidoo0@gmail.com>
2021-12-13 18:11:44 +0000

    remove float type check

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -7,33 +7,33 @@ Rectangle class
 class Rectangle:
     """defines a Rect class"""
     def __init__(self, height, width):
-        self.width = width
-        self.height = height
+        self.__width = width
+        self.__height = height
 
     @property
     def width(self):
         """width getter"""
-        return self.width
+        return self.__width
 
     @width.setter
     def width(self, width):
         """width setter"""
-        if not isinstance(width, int) and not isinstance(width, float):
+        if not isinstance(width, int):
             raise TypeError("width must be an integer")
         if width < 0:
             raise ValueError("width must be >= 0")
-        self.width = width
+        self.__width = width
 
     @property
     def height(self):
         """height getter"""
-        return self.height
+        return self.__height
 
     @height.setter
     def height(self, height):
         """height setter"""
-        if not isinstance(height, int) and not isinstance(height, float):
+        if not isinstance(height, int):
             raise TypeError("width must be an integer")
         if height < 0:
             raise ValueError("height must be >= 0")
-        self.height = height
+        self.__height = height
