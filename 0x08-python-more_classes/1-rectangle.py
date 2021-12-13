a1db52335641dba6b25d7a972dd24adffd767d38
Tonycrz <tonybaidoo0@gmail.com>
2021-12-13 18:13:14 +0000

    remove float type check

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -18,7 +18,7 @@ class Rectangle:
     @width.setter
     def width(self, width):
         """width setter"""
-        if not isinstance(width, int):
+        if type(width) is not int:
             raise TypeError("width must be an integer")
         if width < 0:
             raise ValueError("width must be >= 0")
@@ -32,7 +32,7 @@ class Rectangle:
     @height.setter
     def height(self, height):
         """height setter"""
-        if not isinstance(height, int):
+        if type(height) is not int:
             raise TypeError("width must be an integer")
         if height < 0:
             raise ValueError("height must be >= 0")
