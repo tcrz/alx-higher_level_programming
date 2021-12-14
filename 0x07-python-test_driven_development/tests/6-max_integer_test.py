3958347d444a0fb0d88ad52f8b39935a75a18fe5
Tonycrz <tonybaidoo0@gmail.com>
2021-12-14 12:02:46 +0000

    unit tests

---
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Unittest for max_integer([..])
+"""
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+
+
+class TestMaxInteger(unittest.TestCase):
+    def test_max_integer(self):
+        """tests with max_integer func. should return max integer"""
+        self.assertEqual(max_integer([50, 30, 54]), 54)
+        self.assertEqual(max_integer([50, -30, -54]), 50)
+        self.assertEqual(max_integer([-50, -30, -54]), -30)
+        self.assertEqual(max_integer([-130, 4]), 4)
+
+    def test_errors(self):
+        """tests with max_integer func. should raise TypeError exception"""
+        self.assertRaises(TypeError, max_integer, 4)
+        self.assertRaises(TypeError, max_integer, "Test")
+        self.assertRaises(TypeError, max_integer, ["ds", "sd"])
+        self.assertRaises(TypeError, max_integer, ["ds", []])
+        self.assertRaises(TypeError, max_integer, None)
+
+
+if __name__ - "__main__":
+    unittest.main()
