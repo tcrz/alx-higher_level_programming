8c4630643d53c1e518ec9727c47ef4f73a9bad96
Tonycrz <tonybaidoo0@gmail.com>
2021-12-21 20:37:36 +0000

    10-student.py

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+10-student module
+"""
+
+
+class Student:
+    def __init__(self, first_name, last_name, age):
+        """Initializes the Student instance."""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attr=None)
+    """dictionary representation of a Student instance"""
+    my_dict = dict()
+    if type(attrs) is list and all(type(x) is str for x in attrs):
+        for x in attrs:
+            if x in self.__dict__:
+                my_dict.update({x: self.__dict__[x]})
+            return my_dict
+        return self.__dict__.copy()
