ed998f349d26f47f7765891a4094ac000d3dc575
Tonycrz <tonybaidoo0@gmail.com>
2021-12-21 18:13:38 +0000

    7-add_item.py

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+7-add_item script
+"""
+
+import sys
+import os
+import json
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+
+arglist = []
+for i in sys.argv:
+    if i == sys.argv[0]:
+        pass
+    else:
+        arglist.append(i)
+
+file = "add_item.json"
+if os.path.exists(file):
+    data = load_from_json_file(file)
+    data = data + arglist
+    save_to_json_file(data, file)
+else:
+    save_to_json_file([], file)
