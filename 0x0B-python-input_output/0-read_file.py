7a052aa4681c57bc65e23680dd8b363224a4218a
Tonycrz <tonybaidoo0@gmail.com>
2021-12-21 13:30:45 +0000

    0-read_file.py

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+0-read_file module
+"""
+
+
+def read_file(filename=""):
+    """reads a texts file and prints it to stdout"""
+
+    with open(filename, encoding='utf-8') as file:
+        print(file.read())
