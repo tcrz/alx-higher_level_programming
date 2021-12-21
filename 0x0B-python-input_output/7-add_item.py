01b0cad52382ad75002f9fafa31a06cabdd3e690
Tonycrz <tonybaidoo0@gmail.com>
2021-12-21 18:18:01 +0000

    7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -4,7 +4,6 @@
 """
 import sys
 import os
-import json
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 """
