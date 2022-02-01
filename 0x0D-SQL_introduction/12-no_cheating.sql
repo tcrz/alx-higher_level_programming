ff4d5234700ad257a7b4ac4b12df4dd45e14fa92
Tonycrz <tonybaidoo0@gmail.com>
2022-02-01 19:15:08 +0000

    12-no_cheating.sql

--- 0x0D-SQL_introduction/12-no_cheating.sql
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -1,6 +1,3 @@
 -- updates the score of Bob to 10 in the table second_table
 -- without using Bob's id value, only the name field
-UPDATE second_table t1, second_table t2
-SET t1.score = t2.score
-WHERE t1.name = "Bob" and t2.name = "John";
-
+UPDATE second_table SET score=10 WHERE name='Bob'
