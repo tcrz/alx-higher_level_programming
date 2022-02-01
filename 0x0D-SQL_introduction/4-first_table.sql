613ecb15a69db7e9d03f352cc9d341fc6187e2a2
Tonycrz <tonybaidoo0@gmail.com>
2022-02-01 14:54:14 +0000

    4-first_table.sql

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,5 @@
+-- creates a table 'first table' with attribute names 'id' and ' name'
+-- and their data types
+DROP TABLE IF EXISTS first_table;
+CREATE TABLE first_table(id INT, name VARCHAR(256));
+SHOW TABLES;
