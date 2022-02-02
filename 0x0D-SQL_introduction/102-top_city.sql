3beacffc52abafe2f24a6a2b42ba016d3ecce6f1
Tonycrz <tonybaidoo0@gmail.com>
2022-02-02 18:58:53 +0000

    102-top_city.sql

---
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -0,0 +1,3 @@
+-- Import in hbtn_0c_0 database this table dump: sql file
+-- script that displays the average temperature (Fahrenheit) by city ordered by temperature (descending)
+mysqldump -u root hbtn_0c_0 -p < temperatures.sql
