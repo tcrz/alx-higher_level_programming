f5b97d24a2688224fd224ec5166c09e3c17fecb7
Tonycrz <tonybaidoo0@gmail.com>
2022-02-03 16:22:09 +0000

    102-top_city.sql

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,3 +1,3 @@
 -- Import in hbtn_0c_0 database this table dump: sql file
 -- script that displays the average temperature (Fahrenheit) by city ordered by temperature (descending)
-mysqldump -u root hbtn_0c_0 -p < temperatures.sql
+SELECT city, avg(value) as avg_temp from temperatures WHERE month > 6 AND  month < 9 GROUP BY city HAVING avg_temp > 73 ORDER BY avg_temp DESC;
