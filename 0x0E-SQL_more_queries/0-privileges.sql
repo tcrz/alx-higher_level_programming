e3abd4bf8ed29a58f5889e4ede3079e2ea2c2b04
Tonycrz <tonybaidoo0@gmail.com>
2022-02-03 16:57:16 +0000

    0-privileges.sql

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,5 +1,3 @@
--- lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server
--- lists all granted privileged for one user in a database
+-- lists all privileges of the MySQL users 'user_0d_1' and 'user_0d_2' on your server (in localhost).
 SHOW GRANTS FOR user_0d_1@localhost;
--- lists all granted privileged for one user in a database
 SHOW GRANTS FOR user_0d_2@localhost;
