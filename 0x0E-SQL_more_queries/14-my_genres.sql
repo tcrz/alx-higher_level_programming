5c8d25d263202d8d7f1b0d5f5122fec071a7319b
Tonycrz <tonybaidoo0@gmail.com>
2022-02-24 18:32:05 +0000

    14-my_genres.sql

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -4,7 +4,6 @@
 -- Results must be sorted in ascending order by the genre name
 -- You can use only one SELECT statement
 -- The database name will be passed as an argument of the mysql command
-SELECT tv_genres.name FROM tv_genres
-JOIN tv_show_genres ON tv_genres.id=tv_show_genres.genre_id
-WHERE tv_show_genres.show_id=8
+SELECT tv_genres.name FROM tv_genres, tv_show_genres, tv_shows
+WHERE tv_genres.id=tv_show_genres.genre_id AND tv_show_genres.show_id=tv_shows.id AND tv_shows.title="Dexter"
 ORDER BY tv_genres.name ASC;
