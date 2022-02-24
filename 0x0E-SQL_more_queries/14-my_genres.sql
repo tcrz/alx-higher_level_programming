7a0786750326e6d6a339f8ae24b9ff55432cea0a
Tonycrz <tonybaidoo0@gmail.com>
2022-02-24 14:46:16 +0000

    14-my_genres.sql

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -5,6 +5,6 @@
 -- You can use only one SELECT statement
 -- The database name will be passed as an argument of the mysql command
 SELECT tv_genres.name FROM tv_genres
-JOIN tv_show_genres on tv_genres.id=tv_show_genres.genre_id
+JOIN tv_show_genres ON tv_genres.id=tv_show_genres.genre_id
 WHERE tv_show_genres.show_id=8
 ORDER BY tv_genres.name ASC;
