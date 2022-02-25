b4f5a36e39ccb4d04c634d8d893d622ece9ea2cb
Tonycrz <tonybaidoo0@gmail.com>
2022-02-25 21:13:49 +0000

    101/102advanced

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -1,12 +1,12 @@
--- lists all shows without the genre Comedy in the database hbtn_0d_tvshows
--- uses a database to list all rows not linked to one row
-SELECT title
-FROM tv_shows
-WHERE title NOT IN
-(SELECT title
-FROM tv_shows
-LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
-WHERE tv_genres.name = 'Comedy')
-GROUP BY title
-ORDER BY title ASC;
+-- script that lists all shows without the genre Comedy in the database hbtn_0d_tvshows.
+-- The tv_genres table contains only one record where name = Comedy (but the id can be different)
+-- Each record should display: tv_shows.title
+-- Results must be sorted in ascending order by the show title
+-- You can use a maximum of two SELECT statement
+-- The database name will be passed as an argument of the mysql command
+SELECT tv_shows.title FROM tv_shows
+WHERE title NOT IN (
+	SELECT tv_shows.title from tv_shows, tv_show_genres, tv_genres
+	WHERE tv_shows.id=tv_show_genres.show_id AND tv_show_genres.genre_id=tv_genres.id AND tv_genres.name="Comedy"
+	)
+ORDER BY tv_shows.title ASC;
