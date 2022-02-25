609071adebb82bb158208f3a92d5f5c2a663744a
Tonycrz <tonybaidoo0@gmail.com>
2022-02-25 20:29:37 +0000

    103-rating_genres.sql

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -3,7 +3,7 @@
 -- Results must be sorted in descending order by their rating
 -- You can use only one SELECT statement
 -- The database name will be passed as an argument of the mysql command
-SELECT tv_genres.name, sum(tv_show_ratings.rate) as rating
+SELECT tv_genres.name, sum(tv_show_ratings.rate) AS rating
 FROM tv_genres, tv_show_genres, tv_shows, tv_show_ratings
 WHERE tv_genres.id=tv_show_genres.genre_id
 AND tv_show_genres.show_id=tv_shows.id AND tv_shows.id=tv_show_ratings.show_id
