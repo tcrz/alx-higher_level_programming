21ad017cb13512432e177f338255c1bbdac07158
Tonycrz <tonybaidoo0@gmail.com>
2022-02-28 17:35:30 +0000

    2-arguments.js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -3,7 +3,7 @@ const argv = process.argv.slice(2);
 if (!argv.length) {
   console.log('No argument');
 } else if (argv.length === 1) {
-  console.log('Argument Found');
+  console.log('Argument found');
 } else {
-  console.log('Arguments Found');
+  console.log('Arguments found');
 }
