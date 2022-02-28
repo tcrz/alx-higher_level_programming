a5716c23ec95351ecf0e7e33dd73c765b831a760
Tonycrz <tonybaidoo0@gmail.com>
2022-02-28 22:25:47 +0000

    101-call_me_moby.js

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 exports.callMeMoby = function (x, theFunction) {
-    for (let step = 0; step < x; step++) {
-      theFunction();
-    };
+  for (let step = 0; step < x; step++) {
+    theFunction();
+  };
 };
