82e32f3e4c98661789bc6c10c04ac054b90cbbf8
Tonycrz <tonybaidoo0@gmail.com>
2022-02-28 21:07:47 +0000

    12-object.js

--- 0x12-javascript-warm_up/12-object.js
+++ 0x12-javascript-warm_up/12-object.js
@@ -1,11 +1,12 @@
-#!/usr/bin/node
-const myObject = {
-  type: 'object',
-  value: 12
-};
-console.log(myObject)
-/*
-  MY CODE HERE
-*/
-myObject.value = 89;
+#!/usr/bin/node                                                                                                                         
+const myObject = {                                                                                                                      
+  type: 'object',                                                                                                                       
+  value: 12                                                                                                                             
+};                                                                                                                                      
+console.log(myObject);                                                                                                                  
+/*                                                                                                                                      
+MY CODE HERE                                                                                                                            
+*/                                                                                                                                      
+myObject.value = 89;                                                                                                                    
 console.log(myObject);
+         
