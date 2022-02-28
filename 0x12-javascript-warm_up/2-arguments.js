f4e5e8124772672cf50589231c99878d6ab79bfe
Tonycrz <tonybaidoo0@gmail.com>
2022-02-28 17:13:19 +0000

    2-arguments.js

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const argv = process.argv.slice(2);
+if (!argv.length)
+  console.log('No argument');
+else if (argv.length == 1)
+  console.log('Argument Found');
+else
+  console.log('Arguments Found');
