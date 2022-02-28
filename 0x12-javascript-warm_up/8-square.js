2dbd4324ead5b65807bda5fe7bbefb1e17598171
Tonycrz <tonybaidoo0@gmail.com>
2022-02-28 19:35:15 +0000

    8-square.js

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -7,7 +7,7 @@ if (isNaN(parseInt(argv[0]))) {
   for (let step = 0; step < parseInt(argv[0]); step++) {
     for (let step = 0; step < parseInt(argv[0]); step++) {
       str += 'X';
-  }
+    }
     console.log(str);
     str = '';
   }
