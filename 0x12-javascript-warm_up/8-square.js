4a2cbb1f7f032ab06671bb5399043863e54ce47f
Tonycrz <tonybaidoo0@gmail.com>
2022-02-28 19:32:51 +0000

    8-square.js

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const argv = process.argv.slice(2);
+let str = '';
+if (isNaN(parseInt(argv[0]))) {
+  console.log('Missing sizes');
+} else {
+  for (let step = 0; step < parseInt(argv[0]); step++) {
+    for (let step = 0; step < parseInt(argv[0]); step++) {
+      str += 'X';
+    }
+    console.log(str);
+    str = '';
+  }
+}
