ec04610d346cda911978f737ee5512cd1888b62e
Tonycrz <tonybaidoo0@gmail.com>
2022-03-01 21:17:05 +0000

    100-not_my_genres.sql

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -1,12 +1,14 @@
--- uses the hbtn_0d_tvshows database to list all genres not linked to the show Dexter
--- uses a database to list all rows not linked to one row
-SELECT name
-FROM tv_genres
-WHERE name NOT IN
-(SELECT name
-FROM tv_genres
-LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
-LEFT JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
-WHERE tv_shows.title = 'Dexter')
-GROUP BY name
-ORDER BY name ASC;
+-- script that uses the hbtn_0d_tvshows database to list all genres not linked to the show Dexter
+-- The tv_shows table contains only one record where title = Dexter (but the id can be different)
+-- Each record should display: tv_genres.name
+-- Results must be sorted in ascending order by the genre name
+-- You can use a maximum of two SELECT statement
+-- The database name will be passed as an argument of the mysql command
+SELECT tv_genres.name FROM tv_genres
+WHERE name NOT IN (
+	SELECT tv_genres.name FROM tv_genres
+	JOIN tv_show_genres ON tv_show_genres.genre_id=tv_genres.id
+	JOIN tv_shows ON tv_shows.id=tv_show_genres.show_id
+	WHERE tv_shows.title="Dexter"
+	)
+ORDER BY tv_genres.name DESC;
