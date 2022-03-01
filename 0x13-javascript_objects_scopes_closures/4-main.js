d15ea8a372ff717a2e1fe2e1d4db774a3f0dea16
Tonycrz <tonybaidoo0@gmail.com>
2022-03-01 13:32:40 +0000

    updates

--- 0x13-javascript_objects_scopes_closures/4-main.js
+++ 0x13-javascript_objects_scopes_closures/4-main.js
@@ -7,12 +7,8 @@ r1.print();
 
 console.log('Double:');
 r1.double();
-console.log('----------');
-console.log('Normal:');
 r1.print();
 
 console.log('Rotate:');
 r1.rotate();
-console.log('----------')
-console.log('Normal:');
 r1.print();
--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -19,26 +19,10 @@ module.exports = class Rectangle {
   }
 
   rotate () {
-    let strr = '';
     [this.height, this.width] = [this.width, this.height];
-    for (let step = 0; step < this.height; step++) {
-      for (let step = 0; step < this.width; step++) {
-        strr += 'X';
-      }
-      console.log(strr);
-      strr = '';
-    }
   }
 
   double () {
-    let str = '';
     [this.height, this.width] = [this.height * 2, this.width * 2];
-    for (let step = 0; step < this.height; step++) {
-      for (let step = 0; step < this.width; step++) {
-        str += 'X';
-      }
-      console.log(str);
-      str = '';
-    }
   }
 };
