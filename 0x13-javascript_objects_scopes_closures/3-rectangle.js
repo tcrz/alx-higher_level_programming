f1efdd428c45e3b4675a06e4bc1c65f0f54c982e
Tonycrz <tonybaidoo0@gmail.com>
2022-03-01 12:39:08 +0000

    3-rectangle.js

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let str = '';
+    for (let step = 0; step < this.height; step++) {
+      for (let step = 0; step < this.width; step++) {
+        str += 'X';
+      }
+      console.log(str);
+      str = '';
+    }
+  }
+};
