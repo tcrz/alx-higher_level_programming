13482f04b72b92229f71d17a23aec513c658f31f
Tonycrz <tonybaidoo0@gmail.com>
2022-03-01 20:54:01 +0000

    102-concat.js

---
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const argv = process.argv.slice(2);
+const fs = require('fs');
+fs.readFile(argv[0], 'utf8', function (err, data) {
+  if (err) throw (err)
+  const contents = data;
+  fs.readFile(argv[1], 'utf8', function (err, data) {
+    if (err) throw (err)
+    const contents2 = data;
+    fs.appendFile(argv[2], contents + contents2, function (err) {
+      if (err) throw (err);
+    })
+  })
+});
