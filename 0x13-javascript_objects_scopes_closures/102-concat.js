220644b20c10b33f7b03c83743994aaa19d33c33
Tonycrz <tonybaidoo0@gmail.com>
2022-03-01 20:57:43 +0000

    102-concat.js

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -2,10 +2,10 @@
 const argv = process.argv.slice(2);
 const fs = require('fs');
 fs.readFile(argv[0], 'utf8', function (err, data) {
-  if (err) throw (err)
+  if (err) throw (err);
   const contents = data;
   fs.readFile(argv[1], 'utf8', function (err, data) {
-    if (err) throw (err)
+    if (err) throw (err);
     const contents2 = data;
     fs.appendFile(argv[2], contents + contents2, function (err) {
       if (err) throw (err);
