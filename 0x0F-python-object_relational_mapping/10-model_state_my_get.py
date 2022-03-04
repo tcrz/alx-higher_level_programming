31c416bf6e00d81d5dc6826f89e405b6529d3faf
Tonycrz <tonybaidoo0@gmail.com>
2022-03-04 00:47:20 +0000

    12-model_state_update_id_2.py

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+script that prints the State object with the name passed as argument
+from the database hbtn_0e_6_usa
+
+    - script should take 4 arguments: mysql username, mysql password,
+    database name and state name to search (SQL injection free)
+    - uses the module SQLAlchemy
+    - imports State and Base from model_state
+    - script connects to a MySQL server running on localhost at port 3306
+    - You can assume you have one record with the state name to search
+    - Results must display the states.id
+    - If no state has the name you searched for, display Not found
+"""
+
+if __name__ == '__main__':
+    import sys
+    from sqlalchemy import create_engine, not_, text
+    from sqlalchemy.orm import sessionmaker
+    from model_state import Base, State
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    Session = sessionmaker(bind=engine)
+    s = Session()
+
+    row = s.query(State).filter(State.name == sys.argv[4])[:1]
+    if len(row) == 1:
+        print(row[0].id)
+    else:
+        print("Not Found")
---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+script that changes the name of a State object from the database hbtn_0e_6_usa
+
+    - script should take 3 arguments: mysql username,
+    mysql password and database name
+    - uses the module SQLAlchemy
+    - import State and Base from model_state
+    - script connecta to a MySQL server running on localhost at port 3306
+    - Change the name of the State where id = 2 to New Mexico
+"""
+if __name__ == '__main__':
+    import sys
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+    from model_state import Base, State
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    Session = sessionmaker(bind=engine)
+    s = Session()
+
+    state = s.merge(State(id=2))
+    state.name = "New Mexico"
+    s.commit()
