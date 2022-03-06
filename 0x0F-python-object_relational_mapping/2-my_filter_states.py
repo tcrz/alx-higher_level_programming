abac684094ca3d13124a91d087937a4757a7de9f
Tonycrz <tonybaidoo0@gmail.com>
2022-03-06 01:02:42 +0000

    2-my_filter_states.py

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -21,7 +21,7 @@ if __name__ == '__main__':
     cur.execute("SELECT * FROM states WHERE name='{}' ORDER BY states.id ASC"
                 .format(argv[3]))
     rows = cur.fetchall()
-    if rows is not None:
+    if len(rows) > 0:
         for row in rows:
             print(row)
     else:
