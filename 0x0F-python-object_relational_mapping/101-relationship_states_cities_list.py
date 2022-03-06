dc9a31735527c933310b34692eea7a6e5d758fe2
Tonycrz <tonybaidoo0@gmail.com>
2022-03-06 14:45:46 +0000

    101-relationship_states_cities_list.py

---
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+script that lists all State objects, and corresponding City objects,
+contained in the database hbtn_0e_101_usa
+
+Your script should take 3 arguments: mysql username, mysql password
+and database name
+You must use the module SQLAlchemy
+The connection to your MySQL server must be to localhost on port 3306
+You must only use one query to the database
+You must use the cities relationship for all State objects
+Results must be sorted in ascending order by states.id and cities.id
+"""
+if __name__ == '__main__':
+    import sys
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+    from relationship_state import Base, State
+    from relationship_city import City
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    Session = sessionmaker(bind=engine)
+    s = Session()
+
+    for row in s.query(State).order_by(State.id.asc()):
+        print('{}: {}'.format(row.id, row.name))
+        for city in row.cities:
+            print('\t {}: {}'.format(city.id, city.name))
