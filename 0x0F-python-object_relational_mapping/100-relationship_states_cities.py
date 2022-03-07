d4dad84675a99db96fc832668f7346df20c53c93
Tonycrz <tonybaidoo0@gmail.com>
2022-03-07 08:51:09 +0000

    add_option_to_create_tables

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -21,6 +21,7 @@ if __name__ == '__main__':
 
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     s = Session()
 
@@ -28,6 +29,4 @@ if __name__ == '__main__':
                     state=State(name="California"))
     s.add(new_city)
     s.commit()
-    # city1 = City(name='San Francisco', state=State(name='California'))
-    # s.add(city1)
-    # s.commit()
+
