77826693a15f21e208b17ebbd17af5f7970307e9
Tonycrz <tonybaidoo0@gmail.com>
2022-03-17 21:04:54 +0000

    1-body.sh

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script that takes in a URL, sends a GET request to the URL, and displays the body of the response
-if [ "$(curl -sI -o /dev/null -w "%{http_code}" "$1")" -eq "200" ]; then curl -s "$1"; else echo ""; fi
+curl -sX GET "$1"
