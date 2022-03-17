6322016a7f843f80c03a53c5e426ba65eff5d5c0
Tonycrz <tonybaidoo0@gmail.com>
2022-03-17 20:42:55 +0000

    3-methods.sh

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script that takes in a URL and displays all HTTP methods the server will accept.
-curl -sI -X OPTIONS $1 | grep Allow | awk '{print $2}' | sed -e 's/,/, /g'
+curl -sI -X OPTIONS "$1" | grep Allow | awk '{print $2}' | sed -e 's/,/, /g'
