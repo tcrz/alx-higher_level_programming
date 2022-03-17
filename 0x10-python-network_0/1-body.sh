0b6d0a86c580dac34779fe11724f2a3a8dc7c522
Tonycrz <tonybaidoo0@gmail.com>
2022-03-17 16:07:03 +0000

    1

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 # Bash script that takes in a URL, sends a GET request to the URL, and displays the body of the response
-
 status=$(curl -sI "$1":"$2" | head -n 1 | awk '{print $2}')
-if [ "$status" -eq "200" ]
-then
-curl "$1":"$2"
-fi
+if [ "$status" -eq "200" ]; then curl "$1":"$2"; fi
