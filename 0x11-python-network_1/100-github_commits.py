76e6930afae167d1a2784e40c53b72591e7c2d88
Tonycrz <tonybaidoo0@gmail.com>
2022-03-18 21:59:39 +0000

    100-github_commits.py

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -8,18 +8,11 @@ if __name__ == "__main__":
     import sys
 
     r2 = requests.get(
-        'https://api.github.com/repos/{}/{}/commits?per_page=10'
+        'https://api.github.com/repos/{}/{}/commits'
         .format(sys.argv[1], sys.argv[2]))
     data = r2.json()
     if len(data) > 2:
-        dates = []
-        for i in range(len(data)):
+        for i in range(10):
             raw_data = data[i]
-            dates.append((raw_data.get('commit').get('author').get('date')))
-        dates = sorted(dates, reverse=True)
-        for d in dates:
-            for i in range(len(data)):
-                raw_data = data[i]
-                if d == raw_data.get('commit').get('author').get('date'):
-                    print(raw_data.get('sha')+':',
-                          raw_data.get('commit').get('author').get('name'))
+            print(raw_data.get('sha')+':',
+                  raw_data.get('commit').get('author').get('name'))
