2d57434bfab193f8940c3bbfa21877fb6fce801f
Tonycrz <tonybaidoo0@gmail.com>
2022-03-18 20:55:18 +0000

    100-github_commits.py

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -5,11 +5,14 @@ the repository “rails” by the user “rails”
 """
 if __name__ == "__main__":
     import requests
+    import sys
 
     r2 = requests.get(
-        'https://api.github.com/repos/rails/rails/commits?per_page=10')
+        'https://api.github.com/repos/{}/{}/commits?per_page=10'
+        .format(sys.argv[1], sys.argv[2]))
     data = r2.json()
-    for i in range(len(data)):
-        raw_data = data[i]
-        print(raw_data.get('sha'),
-              raw_data.get('commit').get('author').get('name'))
+    if len(data) > 2:
+        for i in range(len(data)):
+            raw_data = data[i]
+            print(raw_data.get('sha'),
+                  raw_data.get('commit').get('author').get('name'))
