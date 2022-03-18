fe661e60dd1728f2b6c91484f301c3e93e5d6c23
Tonycrz <tonybaidoo0@gmail.com>
2022-03-18 12:31:19 +0000

    updates

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 """script that fetches 'https://alx-intranet.hbtn.io/status' """
-import urllib.request
+if __name__ == "__main__":
+    import urllib.request
 
-with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
-    html = response.read()
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as
+    response:
+        html = response.read()
     print("""Body response:\n    - type: {}\n    - content: {}
     - utf8 content: {}""".format(type(html), html, html.decode('utf-8')))
--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """script that takes in a URL, sends a request to the URL and displays
 the value of the X-Request-Id variable found in the header of the response."""
+if __name__ == "__main__":
+    import urllib.request
+    import sys
 
-import urllib.request
-import sys
-
-with urllib.request.urlopen(sys.argv[1]) as response:
-    print(response.getheader('X-Request-Id'))
+    with urllib.request.urlopen(sys.argv[1]) as response:
+        print(response.getheader('X-Request-Id'))
