d3216bb8aa4b45c45e2938be188b83fc8a394cd9
Tonycrz <tonybaidoo0@gmail.com>
2022-03-18 14:25:12 +0000

    6-post_email.py

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+script that takes in a URL and an email address, sends a POST request to the
+passed URL with the email as a parameter, and finally displays the body
+of the response
+"""
+if __name__ == "__main__":
+    import requests
+    import sys
+
+    value_dict = {'email': sys.argv[1]}
+    r = requests.post(sys.argv[0], data=value_dict)
+    print(r.text)
