f20aedbb71a1365e53902f187d376354f61233a2
Tonycrz <tonybaidoo0@gmail.com>
2022-03-18 11:46:37 +0000

    0-hbtn_status.py

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""script that fetches 'https://alx-intranet.hbtn.io/status' """
+import urllib.request
+
+with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+    html = response.read()
+print("Body response:\n    - type: {}\n    - content: {}\n    - utf8 content: {}"
+      .format(type(html), html, html.decode('utf-8')))
