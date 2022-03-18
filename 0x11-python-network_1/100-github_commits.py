c60368a1b77956063bfdbcccf40909e66d89ad32
Tonycrz <tonybaidoo0@gmail.com>
2022-03-18 22:08:17 +0000

    100-github_commits.py

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -7,11 +7,11 @@ if __name__ == "__main__":
     import requests
     import sys
 
-    r = requests.get('https://api.github.com/repos/{}/{}/commits'
-                     .format(sys.argv[2], sys.argv[1]))
-    if r.status_code >= 400:
-        print('None')
-    else:
-        for com in r.json()[:10]:
-            print("{}: {}".format(com.get('sha'),
-                                  com.get('commit').get('author').get('name')))
+    r2 = requests.get(
+        'https://api.github.com/repos/{}/{}/commits?per_page=10'
+        .format(sys.argv[1], sys.argv[2]))
+    data = r2.json()
+    if len(data) > 2:
+        for i in r2.json():
+            print(i.get('sha')+':',
+                  i.get('commit').get('author').get('name'))
