06738d1b5d4be1097313243e58846720c0e9a0af
Tonycrz <tonybaidoo0@gmail.com>
2022-03-18 12:28:30 +0000

    docstring

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""script that takes in a URL, sends a request to the URL and displays
+the value of the X-Request-Id variable found in the header of the response."""
+
 import urllib.request
 import sys
 
