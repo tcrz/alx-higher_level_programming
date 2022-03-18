29730f288cef54b6ad7e0101371ed446ae4b5e94
Tonycrz <tonybaidoo0@gmail.com>
2022-03-18 12:38:25 +0000

    indentation

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,8 +3,8 @@
 if __name__ == "__main__":
     import urllib.request
 
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as
-    response:
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') \
+            as response:
         html = response.read()
     print("""Body response:\n    - type: {}\n    - content: {}
     - utf8 content: {}""".format(type(html), html, html.decode('utf-8')))
