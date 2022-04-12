a2837efa3e7b61b88e1e52a393a204a3ce0e06ca
Tonycrz <tonybaidoo0@gmail.com>
2022-04-12 12:36:47 +0000

    read_n_print

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+/*
+script that reads and prints the content of a file.
+
+The first argument is the file path
+The content of the file must be read in utf-8
+If an error occurred during the reading, print the error object
+*/
+const fs = require('fs');
+const argv = process.argv.slice(2);
+
+fs.readFile(argv[0], 'utf-8', (err, data) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(data);
+  }
+});
