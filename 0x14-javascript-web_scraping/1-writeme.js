16b45919b0dd42f66849bde7293c7fba7533cfe0
Tonycrz <tonybaidoo0@gmail.com>
2022-04-12 12:45:30 +0000

    update_comments

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -3,7 +3,8 @@
 script that reads and prints the content of a file.
 
 The first argument is the file path
-The content of the file must be read in utf-8
+The second argument is the string to write
+The content of the file must be written in utf-8
 If an error occurred during the reading, print the error object
 */
 const fs = require('fs');
