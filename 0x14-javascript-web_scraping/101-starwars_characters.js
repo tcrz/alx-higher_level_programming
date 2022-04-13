bc951e71ee8154997e4e759ad076a3520f6188c2
Tonycrz <tonybaidoo0@gmail.com>
2022-04-13 17:14:55 +0000

    101-starwars_characters.js

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -6,25 +6,31 @@ The first argument is the Movie ID - example: 3 = “Return of the Jedi”
 Display one character name by line in the same order of the list “characters” in the /films/ response
 You must use the Star wars API
 You must use the module request
-"""
 */
 const request = require('request');
 const argv = process.argv.slice(2);
 
 const endpoint = 'https://swapi-api.hbtn.io/api/films/' + argv[0];
 
-request(endpoint, function (error, respose, body) {
+function orderedRequest (array, index) {
+  if (index === array.length) {
+    return;
+  }
+  request(array[index], function (error, respose, body) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log(JSON.parse(body).name);
+      orderedRequest(array, index + 1);
+    }
+  });
+}
+
+request(endpoint, function (error, response, body) {
   if (error) {
     console.log(error);
   } else {
-    JSON.parse(body).characters.forEach(url => {
-      request(url, function (error, respose, body) {
-        if (error) {
-          console.log(error);
-        } else {
-          console.log(JSON.parse(body).name);
-        }
-      });
-    });
+    const chrList = JSON.parse(body).characters;
+    orderedRequest(chrList, 0);
   }
 });
