89ad384e05dca9505a97870f0d9ea2cf78f5f7c7
Tonycrz <tonybaidoo0@gmail.com>
2022-04-13 08:04:37 +0000

    change_mode

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 /*
-script that prints the number of movies where the character “Wedge Antilles” is present.
+Script that prints the number of movies where the character “Wedge Antilles” is present.
 
 The first argument is the API URL of the Star wars API: https://swapi-api.hbtn.io/api/films/
 Wedge Antilles is character ID 18 - your script must use this ID for filtering the result of the API
--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,14 +1,10 @@
 #!/usr/bin/node
 /*
 script that gets the contents of a webpage and stores it in a file.
-
 The first argument is the URL to request
-
 The second argument the file path to store the body response
-
 The file must be UTF-8 encoded
-
- must use the module request
+use the module request
 */
 const request = require('request');
 const fs = require('fs');
