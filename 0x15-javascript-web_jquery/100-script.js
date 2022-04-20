d37d76a551087beb63b15bfdfae16f502c9144e7
Tonycrz <tonybaidoo0@gmail.com>
2022-04-20 14:44:09 +0000

    script that updates the text color of the <header> element to red(script must be imported from the head)

--- 0x15-javascript-web_jquery/100-script.js
+++ 0x15-javascript-web_jquery/100-script.js
@@ -1,3 +1,8 @@
+/*script that updates the text color of the <header> element to red (#FF0000):
+You must use document.querySelector to select the HTML tag
+You can’t use the jQuery API
+Note: Your script must be imported from the <head> tag, not at the end of the HTML
+*/
 window.onload = function toRed () {
   document.querySelector('header').style.color = 'red';
 };
