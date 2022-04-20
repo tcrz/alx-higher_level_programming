6799e3acc6f45b73eb43b241fdea425bb10bcd43
Tonycrz <tonybaidoo0@gmail.com>
2022-04-20 16:00:31 +0000

    fetch translation on click or when ENTER is pressed

---
+++ 0x15-javascript-web_jquery/103-script.js
@@ -0,0 +1,20 @@
+/* JavaScript script that fetches and prints how to say “Hello” depending on the language
+when button/ENTER is pressed */
+$(document).ready(function () {
+  $('INPUT#btn_translate').click(function () {
+    const langCode = $('INPUT#language_code').val();
+    $.get('https://fourtonfish.com/hellosalut/?lang=' + langCode, function (data) {
+      $('DIV#hello').text(data.hello);
+      console.log(data.hello);
+    });
+  });
+  $('INPUT#language_code').on('keypress', function (e) {
+    const langCode = $('INPUT#language_code').val();
+    if (e.which === 13) {
+      $.get('https://fourtonfish.com/hellosalut/?lang=' + langCode, function (data) {
+        $('DIV#hello').text(data.hello);
+        console.log(data.hello);
+      });
+    }
+  });
+});
