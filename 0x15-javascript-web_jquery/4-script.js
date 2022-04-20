4f32f10022e34cea1a078eaafef59b2d148ea2af
Tonycrz <tonybaidoo0@gmail.com>
2022-04-20 12:11:31 +0000

    toggles the class of the <header> element when the user clicks on the tag DIV#toggle_header

---
+++ 0x15-javascript-web_jquery/4-script.js
@@ -0,0 +1,7 @@
+/*
+script that toggles the class of the <header> element when the user clicks on the tag DIV#toggle_header:
+  - The <header> element must always have one class: red or green, never both in the same time and never empty.
+  - If the current class is red, when the user click on DIV#toggle_header, the class must be
+    updated to green ; and the reverse.
+*/
+$('#toggle_header').click(() => $('header').toggleClass('green red'));
